118ca51b97f0ce32812c92d4adfc95d52b2e18ce
Benwebdev <hello@benline.co.uk>
2023-11-08 01:07:52 +0000

    🐛 fix(__init__.py): fix encrypt_bundle_with_password function to properly encrypt Git bundle using OpenSSL ✨ feat(__init__.py): add encrypt_bundle_with_password function to encrypt Git bundle with a password using OpenSSL 🔧 chore(test.py): add test case for encrypt_bundle_with_password function to ensure proper encryption of Git bundle

--- pygitbundle/__init__.py
+++ pygitbundle/__init__.py
@@ -26,3 +26,18 @@ def unpack_bundle(bundle_file, repo_path):
         print('Git bundle unpacked successfully.')
     except subprocess.CalledProcessError as e:
         print(f'Error unpacking Git bundle: {e.stderr}')
+
+def encrypt_bundle_with_password(bundle_file, repo_path, password):
+    encrypt_command = ['git', 'bundle', 'create', bundle_file, '--all']
+
+    try:
+        # Create the Git bundle without encryption
+        subprocess.run(encrypt_command, cwd=repo_path, check=True)
+
+        # Encrypt the bundle using OpenSSL
+        encrypt_command = ['openssl', 'aes-256-cbc', '-salt', '-in', bundle_file, '-out', bundle_file + '.enc']
+        subprocess.run(encrypt_command, cwd=repo_path, check=True, input=password.encode())
+
+        print('Git bundle encrypted successfully.')
+    except subprocess.CalledProcessError as e:
+        print(f'Error encrypting Git bundle: {e.stderr}')
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from pygitbundle import MY_CUSTOM_VAR, MY_CUSTOM_VAR2, create_bundle, verify_bundle, unpack_bundle
+from pygitbundle import create_bundle, verify_bundle, unpack_bundle, encrypt_bundle_with_password
 
 # Test create_bundle
 def test_create_bundle():
@@ -13,11 +13,17 @@ def test_unpack_bundle():
     unpack_bundle('test.bundle', '.')
     assert True
 
+def test_encrypt_bundle_with_password():
+    encrypt_bundle_with_password('test.bundle', '.', 'password')
+    assert True
+
 # Run on main python
 def main():
     test_create_bundle()
     test_verify_bundle()
     test_unpack_bundle()
+    test_encrypt_bundle_with_password()
+
 
 if __name__ == "__main__":
     main()
